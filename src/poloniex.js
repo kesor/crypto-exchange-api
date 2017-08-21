440e8adce2113a49cf08e171c384e9cb57151ab0
Evgeny Zislis <evgeny.zislis@gmail.com>
2017-08-21 03:48:24 +0300

    implement a bunch of trading api tests

--- src/poloniex.js
+++ src/poloniex.js
@@ -66,22 +66,66 @@ export class Poloniex {
   }
 
   // Trading API Methods
-  // async returnBalances() {
-  //   return this._post({ command 'returnBalances' })
+  async returnBalances() {
+    return this._post({ command: 'returnBalances' })
+  }
+
+  async returnCompleteBalances(all?: boolean) {
+    let req : { command: string, account?: string } = { command: 'returnCompleteBalances' }
+    if (all) {
+      Object.assign(req, { "account": "all" })
+    }
+    return this._post(req)
+  }
+
+  async returnDepositAddresses() {
+    return this._post({ command: 'returnDepositAddresses' })
+  }
+
+  async generateNewAddress(currency: string) {
+    return this._post({ command: 'generateNewAddress', currency: currency })
+  }
+
+  async returnDepositsWithdrawals(startDate: number, endDate: number) {
+    return this._post({ command: "returnDepositsWithdrawals",
+      start: Math.floor(startDate/1000),
+      end: Math.floor(endDate/1000)
+    })
+  }
+
+  async returnOpenOrders(currencyPair: "all" | string) {
+    return this._post({ command: "returnOpenOrders", currencyPair: currencyPair })
+  }
+
+  // async returnTradeHistory(currencyPair: "all" | string, startDate?: number, endDate?: number) {
+  //   let req : { command: string, currencyPair?: string, start?: number, end?: number
+  //     } = { command: "returnTradeHistory", currencyPair: currencyPair }
+  //   if (startDate)
+  //     req["start"] = Math.floor(startDate/1000)
+  //   if (endDate)
+  //     req["end"] = Math.floor(endDate/1000)
+  //   return this._post(req)
   // }
 
   // Helper methods
   async _post(query?: {
-    command: string
+    command: string,
+    account?: string,
+    currency?: string,
+    currencyPair?: string,
+    start?: number,
+    end?: number
   }) {
     let that = this
     let params = query ? new URLSearchParams(query).toString() : '';
     return new Promise( (resolve, reject) => {
+
       let ts = new Date().getTime();
       that.invocations.push(ts);
       that.invocations = that.invocations.filter((d) => {
         return d > ts - 1000 // filter-out all the invocations happened more than 1s ago
       })
+      let nonce = (ts * 100 - 1) + that.invocations.filter((d) => ts == d).length
       if (that.invocations.length >= that.maxTrades + 1) {
         return reject(new Error(`restricting requests to Poloniex to maximum of ${that.maxTrades} per second`))
       }
@@ -97,19 +141,26 @@ export class Poloniex {
           'User-Agent': 'github.com/kesor/crypto-exchange-api v0.0.1',
           'Content-Type': 'application/x-www-form-urlencoded',
           'Content-Length': Buffer.byteLength(postData),
-          'Nonce': +ts,
+          'Nonce': nonce,
           'Key': that.key,
           'Sign': crypto.createHmac('sha512', that.secret).update(postData).digest('hex')
         }
       }, (res) => {
         let rawData =''
+        if (res.statusCode < 200 || res.statusCode > 299) {
+          return reject(new Error(`Failed to load page, status code: ${res.statusCode}`))
+        }
         res.on('data', chunk => rawData += chunk)
         res.on('end', () => {
           let response = JSON.parse(rawData)
+          if (response.error) {
+            return reject(new Error(`Poloniex failure: ${response.error}`))
+          }
           return resolve(response);
         })
       })
       req.write(postData)
+      req.on('error', reject)
       req.end();
     })
 
@@ -146,7 +197,7 @@ export class Poloniex {
         }
       }, (res) => {
         if (res.statusCode < 200 || res.statusCode > 299) {
-          reject(new Error(`Failed to load page, status code: ${res.statusCode}`))
+          return reject(new Error(`Failed to load page, status code: ${res.statusCode}`))
         }
         let rawData = ''
         res.on('data', (chunk) => { rawData += chunk; });
--- test/poloniex.test.js
+++ test/poloniex.test.js
@@ -255,44 +255,144 @@ describe('Poloniex', () => {
     })
     it('should send a nonce on each request', async () => {
       sandbox.useFakeTimers(new Date())
-      scope.matchHeader('nonce', sandbox.clock.now.toString())
+      scope.matchHeader('nonce', (sandbox.clock.now * 100).toString())
         .post(pathname)
         .reply(200, {})
       t.deepEqual({}, await plx._post())
     })
-    // validate that the nonce never repeats itself ...
-    // use timestamp for nonce - but still do increment it
-    it('should increment the nonce on each subsequent request')
-    it('should raise an error on poloniex errors')
-    it('should raise an error on http connection errors')
-    it('should raise an error on status codes other that 2xx')
+    it('should never repeat the same nonce twice', async () => {
+      sandbox.useFakeTimers(new Date())
+      scope.post(pathname).matchHeader('nonce', (sandbox.clock.now * 100).toString()).reply(200, {})
+      scope.post(pathname).matchHeader('nonce', (sandbox.clock.now * 100 + 1).toString()).reply(200, {})
+      await plx._post()
+      await plx._post()
+    })
+    it('should raise an error on poloniex errors', (done) => {
+      scope.post(pathname).reply(200, { error: 'poloniex has problems' })
+      plx._post().catch( (result) => {
+        t.equal('Error: Poloniex failure: poloniex has problems', result)
+        done()
+      })
+    })
+    it('should raise an error on http connection errors', (done) => {
+      scope.post(pathname).replyWithError('request error')
+      plx._post().catch( (result) => {
+        t.equal('Error: request error', result)
+        done()
+      })
+    })
+    it('should raise an error on status codes other that 2xx', (done) => {
+      scope.post(pathname).reply(404, '{ "error": "Not found" }')
+      plx._post().catch( (result) => {
+        t.equal('Error: Failed to load page, status code: 404', result)
+        done()
+      })
+    })
   })
   describe('trading api commands', () => {
-    let plx, scope, pathname
+    let plx, fakePost, startDate, endDate;
     let key = 'public key'
     let secret = 'very secret part that is private'
     beforeEach(() => {
-      pathname = URL_TRADING_API.pathname
       nock.disableNetConnect()
       plx = new Poloniex(key, secret)
-      scope = nock(URL_TRADING_API.origin)
+      fakePost = sandbox.stub(plx, '_post')
+      startDate = new Date();
+      startDate.setFullYear(startDate.getFullYear() - 1)
+      endDate = new Date();
     })
     afterEach(() => {
-      nock.cleanAll()
-      sandbox.reset()
+      sandbox.reset();
     })
-    it('should implement returnBalances')
-    it('should implement returnCompleteBalances')
-    it('should allow to ask for returnCompleteBalances on exchange account')
-    it('should allow to ask for returnCompleteBalances on margin account')
-    it('should allow to ask for returnCompleteBalances on lending account')
-    it('should implement returnDepositAddresses')
-    it('should implement generateNewAddress for a specified currency')
-    it('should implement returnDepositsWithdrawals between start and end unix timestamps')
-    it('should implement returnOpenOrders for all currencies')
-    it('should implement returnOpenOrders for a provided currencyPair')
-    it('should implement returnTradeHistory for a all currencies')
-    it('should implement returnTradeHistory for a provided currencyPair')
+    it('should implement returnBalances', async () => {
+      let res = {"BTC":"0.59098578","LTC":"3.31117268"}
+      fakePost.returns(res);
+      t.deepEqual(res, await plx.returnBalances())
+      t.ok(fakePost.calledOnce)
+      t.ok(fakePost.calledWith({ command: 'returnBalances' }))
+    })
+    it('should implement returnCompleteBalances', async () => {
+      let res = {
+        "LTC":{"available":"5.015","onOrders":"1.0025","btcValue":"0.078"},
+        "NXT":{"available":"5.015","onOrders":"1.0025","btcValue":"0.078"}
+      }
+      fakePost.returns(res)
+      t.deepEqual(res, await plx.returnCompleteBalances())
+      t.ok(fakePost.calledOnce)
+      t.ok(fakePost.calledWith({ command: 'returnCompleteBalances' }))
+    })
+    it('should allow to ask for returnCompleteBalances on exchange account', async () => {
+      // TODO: check what the REAL response is
+      let res = {
+        "exchange": {
+          "LTC":{"available":"5.015","onOrders":"1.0025","btcValue":"0.078"},
+          "NXT":{"available":"5.015","onOrders":"1.0025","btcValue":"0.078"}
+        }
+      }
+      fakePost.returns(res)
+      t.deepEqual(res, await plx.returnCompleteBalances(true))
+      t.ok(fakePost.calledOnce)
+      t.ok(fakePost.calledWith({ command: 'returnCompleteBalances', "account": "all" }))
+    })
+    it('should implement returnDepositAddresses', async () => {
+      let res = {"BTC":"19YqztHmspv2egyD6jQM3yn81x5t5krVdJ","LTC":"LPgf9kjv9H1Vuh4XSaKhzBe8JHdou1WgUB","ITC":"Press Generate.."}
+      fakePost.returns(res)
+      t.deepEqual(res, await plx.returnDepositAddresses())
+      t.ok(fakePost.calledOnce)
+      t.ok(fakePost.calledWith({ command: "returnDepositAddresses" }))
+    })
+    it('should implement generateNewAddress for a specified currency', async () => {
+      let res = {"success":1,"response":"CKXbbs8FAVbtEa397gJHSutmrdrBrhUMxe"}
+      fakePost.returns(res)
+      t.deepEqual(res, await plx.generateNewAddress('BTC'))
+      t.ok(fakePost.calledOnce)
+      t.ok(fakePost.calledWith({ command: "generateNewAddress", "currency": "BTC" }))
+    })
+    it('should implement returnDepositsWithdrawals between start and end unix timestamps', async () => {
+      let res = {"deposits": [{"currency":"BTC","address":"xxx","amount":"0.01006132","confirmations":10,
+      "txid":"17f819a91369a9ff6c4a34216d434597cfc1b4a3d0489b46bd6f924137a47701","timestamp":1399305798,"status":"COMPLETE"},{"currency":"BTC","address":"yyy","amount":"0.00404104","confirmations":10,
+      "txid":"7acb90965b252e55a894b535ef0b0b65f45821f2899e4a379d3e43799604695c","timestamp":1399245916,"status":"COMPLETE"}],
+      "withdrawals":[{"withdrawalNumber":134933,"currency":"BTC","address":"1N2i5n8DwTGzUq2Vmn9TUL8J1vdr1XBDFg","amount":"5.00010000",
+      "timestamp":1399267904,"status":"COMPLETE: 36e483efa6aff9fd53a235177579d98451c4eb237c210e66cd2b9a2d4a988f8e","ipAddress":"127.0.0.1"}]}
+      fakePost.returns(res)
+      t.deepEqual(res, await plx.returnDepositsWithdrawals(startDate, endDate))
+      t.ok(fakePost.calledOnce)
+      t.ok(fakePost.calledWith({ command: "returnDepositsWithdrawals", "start": Math.floor(startDate/1000), "end": Math.floor(endDate/1000) }))
+    })
+    it('should implement returnOpenOrders for a provided currencyPair', async () => {
+      let res = [{"orderNumber":"120466","type":"sell","rate":"0.025","amount":"100","total":"2.5"},{"orderNumber":"120467","type":"sell","rate":"0.04","amount":"100","total":"4"}]
+      fakePost.returns(res)
+      t.deepEqual(res, await plx.returnOpenOrders("BTC_XCP"))
+      t.ok(fakePost.calledOnce)
+      t.ok(fakePost.calledWith({ "command": "returnOpenOrders", "currencyPair": "BTC_XCP" }))
+    })
+    // it('should implement returnTradeHistory for a all currencies', async () => {
+    //   let res = [{ "globalTradeID": 25129732, "tradeID": "6325758", "date": "2016-04-05 08:08:40", "rate": "0.02565498", "amount": "0.10000000", "total": "0.00256549", "fee": "0.00200000", "orderNumber": "34225313575", "type": "sell", "category": "exchange" }, { "globalTradeID": 25129628, "tradeID": "6325741", "date": "2016-04-05 08:07:55", "rate": "0.02565499", "amount": "0.10000000", "total": "0.00256549", "fee": "0.00200000", "orderNumber": "34225195693", "type": "buy", "category": "exchange" }]
+    //   fakePost.returns(res)
+    //   t.deepEqual(res, await plx.returnTradeHistory("BTC_XCP"))
+    //   t.ok(fakePost.calledOnce)
+    //   t.ok(fakePost.calledWith({ "command": "returnTradeHistory", "currencyPair": "BTC_XCP" }))
+    // })
+    // it('should implement returnTradeHistory with a startDate parameter', async () => {
+    //   let res = [{ "globalTradeID": 25129732, "tradeID": "6325758", "date": "2016-04-05 08:08:40", "rate": "0.02565498", "amount": "0.10000000", "total": "0.00256549", "fee": "0.00200000", "orderNumber": "34225313575", "type": "sell", "category": "exchange" }, { "globalTradeID": 25129628, "tradeID": "6325741", "date": "2016-04-05 08:07:55", "rate": "0.02565499", "amount": "0.10000000", "total": "0.00256549", "fee": "0.00200000", "orderNumber": "34225195693", "type": "buy", "category": "exchange" }]
+    //   fakePost.returns(res)
+    //   t.deepEqual(res, await plx.returnTradeHistory("BTC_XCP", startDate))
+    //   t.ok(fakePost.calledOnce)
+    //   t.ok(fakePost.calledWith({
+    //     command: "returnTradeHistory", currencyPair: "BTC_XCP",
+    //     start: Math.floor(startDate/1000)
+    //   }))
+    // })
+    // it('should implement returnTradeHistory with a startDate and endDate parameters', async () => {
+    //   let res = [{ "globalTradeID": 25129732, "tradeID": "6325758", "date": "2016-04-05 08:08:40", "rate": "0.02565498", "amount": "0.10000000", "total": "0.00256549", "fee": "0.00200000", "orderNumber": "34225313575", "type": "sell", "category": "exchange" }, { "globalTradeID": 25129628, "tradeID": "6325741", "date": "2016-04-05 08:07:55", "rate": "0.02565499", "amount": "0.10000000", "total": "0.00256549", "fee": "0.00200000", "orderNumber": "34225195693", "type": "buy", "category": "exchange" }]
+    //   fakePost.returns(res)
+    //   t.deepEqual(res, await plx.returnTradeHistory("BTC_XCP", startDate, endDate))
+    //   t.ok(fakePost.calledOnce)
+    //   t.ok(fakePost.calledWith({
+    //     command: "returnTradeHistory", currencyPair: "BTC_XCP",
+    //     start: Math.floor(startDate/1000), end: Math.floor(endDate/1000)
+    //   }))
+    // })
     it('should implement returnOrderTrades for a provided orderNumber')
     it('should implement buy for currencyPair at the rate and amount specified')
     it('should implement sell for currencyPair at the rate and amount specified')
